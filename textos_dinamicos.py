5f42f698d32283b7d9ebfa596c6d9d8ecca30a14
carlosmcastro <56790554+carlosmcastro@users.noreply.github.com>
2020-03-02 21:52:05 -0300

    Ahora no solo gestiona los textos dinamicos.
    Ahora posee una función que gestiona la legitimidad de los archivos.

--- textos_dinamicos.py
+++ textos_dinamicos.py
@@ -1,5 +1,8 @@
+#encoding: utf-8
 import os, pickle
 import pandas as pd
+import hashlib
+from urllib.request import urlopen
 
 data=pd.read_csv('astronomical_data.csv')
 #Data from binary stellar systems have been cleaned.
@@ -7,10 +10,43 @@ data=data.drop(data.loc[data['pl_cbflag']==1].index)
 
 #en base a los archivos en la carpeta de texto variable, actualiza los textos correspondientes.
 def update():
-	for t in os.listdir('textos_variables'):
+	txt_din = [i for i in os.listdir('textos_variables') if '.pickle' in i]
+	for t in txt_din:
 		with open(f'textos_variables/{t}', 'rb') as f:
 			texto_mapa = pickle.load(f)
 		texto_calculos = eval(texto_mapa['constantes'])
 		with open(f'textos/{t.replace("pickle", "txt")}', "w", encoding = "utf-8") as f:
 			f.write(texto_mapa['titulo']+'\n')
 			f.write(texto_mapa['contenido'].format(*eval(texto_mapa['variables'].format(*[f'texto_calculos[{i}]' for i, u in enumerate(texto_calculos)]))))
+			
+#comprueba si los archivos estan disponibles y si no estan corruptos.
+def calidad(legi, direc):
+	raw = "https://github.com/carlosmcastro/PlanetAPP-BACKEND-/blob/master/{}?raw=true"
+	legi = f"{direc}/{legi}"
+	
+	with urlopen(raw.format(legi)) as response:
+		hashes = pickle.loads(response.read())
+		
+	def escribir_(d_arch):
+		with urlopen(raw.format(d_arch)) as response:
+			with open(d_arch, "wb") as f:
+				f.write(response.read())
+				
+	def hash_comp(d_arch):
+		m=hashlib.md5()
+		with open(d_arch, "rb") as f:
+			m.update(f.read())		
+		return m.hexdigest()
+		
+	for arch, hex_md5 in hashes.items():
+		d_arch = f"{direc}/{arch}"
+		if arch in os.listdir('textos'):
+			if hash_comp(d_arch) != hex_md5:
+				escribir_(d_arch)
+		else:
+			escribir_(d_arch)
+	
+#legitimidad de los archivos internos.
+def legitimidad():
+	calidad('legitimidad_textos.dat', 'textos')
+	calidad('legitimidad_textos_variables.dat', 'textos_variables')
