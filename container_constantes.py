9dae46994be46c2a329963359afeb611957a17b4
carlosmcastro <56790554+carlosmcastro@users.noreply.github.com>
2020-03-03 04:35:14 -0300

    Selección y busqueda de datos.
    añadido diccionario para mantener el lenguaje humano

--- container_constantes.py
+++ container_constantes.py
@@ -7,3 +7,12 @@ TABLAS_REF = "data.pickle"
 
 TXT_STATIC = 'textos'
 TXT_DINAMIC = 'textos_variables'
+
+EQUIVALENCIAS = {'clasificacion_estrella': 'st_spstr', 'instalacion_descubrimiento_planeta': 'pl_facility',
+		  'lugar_descubrimiento_planeta': 'pl_locale', 'telescopio_descubrimiento_planeta': 'pl_telescope', 
+		  'intrumento_descubrimiento_planeta': 'pl_instrument', 'distancia_estrella': 'st_dist', 
+		  'edad_estrella': 'st_age', 'numero_planetas_estrella': 'pl_pnum', 
+		  'numero_lunas_planeta': 'pl_mnum', 'año_descubrimiento_planeta': 'pl_disc',
+		  'masa_planeta': 'pl_bmassj', 'radio_planeta': 'pl_rade', 
+		  'densidad_planeta': 'pl_dens', 'nombre_planeta': 'pl_name',
+		  'nombre_estrella': 'pl_hostname'}
--- parsetext.py
+++ parsetext.py
@@ -1,42 +1,50 @@
 #encoding: utf-8
 
-from container_constantes import ASTRO_DATA
+from container_constantes import ASTRO_DATA, EQUIVALENCIAS
 import pandas as pd
 
 data=pd.read_csv(ASTRO_DATA)
 #Data from binary stellar systems have been cleaned.
 data=data.drop(data.loc[data['pl_cbflag']==1].index)
 
-#Busca los planetas que cumplen la condición exacta.
-#Ejemplo: whopl_equal(clasificacion_estrella='K2 IV')
-def whopl_equal(**kargs):
+#Busquedador principal.
+def whopl(who_type, elec, elec_filt):
 	filt_data=data.copy() #copia
+	elecciones = {EQUIVALENCIAS[i] : i for i in elec}
+	
+	if who_type=='equal':
+		for i in elec_filt:
+			filt_data=filt_data[filt_data[EQUIVALENCIAS[i]]==elec_filt[i]]
+	else:
+		for i in elec_filt:
+			filt_data=filt_data[(filt_data[EQUIVALENCIAS[i]]>elec_filt[i][0]) & (filt_data[EQUIVALENCIAS[i]]<elec_filt[i][1])]
 
-	equivalencia = {'clasificacion_estrella': 'st_spstr', 'instalacion_descubrimiento_planeta': 'pl_facility',
-			  'lugar_descubrimiento_planeta': 'pl_locale', 'telescopio_descubrimiento_planeta': 'pl_telescope', 
-			  'intrumento_descubrimiento_planeta': 'pl_instrument'}
-	
-	for i in kargs:
-		filt_data=filt_data[filt_data[equivalencia[i]]==kargs[i]]
-	
-	return filt_data.pl_name.to_list()
+	return filt_data[elecciones.keys()].drop_duplicates().rename(columns=elecciones).to_dict('list')
+
+#Busca los planetas que cumplen la condición exacta.
+#Ejemplo: whopl_equal('nombre_planeta', 'edad_estrella', edad_estrella=1.60)
+def whopl_equal(*args ,**kargs):
+	return whopl('equal', args, kargs)
 	
 #Busca los planetas que cumplen la condición dentro de un intervalo.
-#Ejemplo 1: whopl_inequal(edad_estrella=(1,2), distancia_estrella=(2, 25))
-#Ejemplo 2: whopl_inequal(distancia_estrella=(7000, float('inf')))
+#Ejemplo 1: whopl_inequal('nombre_planeta', 'distancia_estrella', edad_estrella=(1,2), distancia_estrella=(2, 25))
+#Ejemplo 2: whopl_inequal('nombre_planeta', 'distancia_estrella', distancia_estrella=(7000, float('inf')))
 #Se debe usar float('-inf') ó float('inf') para establecer una cota superior o inferior.
-def whopl_inequal(**kargs):
-	filt_data = data.copy()
+def whopl_inequal(*args, **kargs):
+	return whopl('inequal', args, kargs)
 	
-	equivalencia = {'distancia_estrella': 'st_dist', 'edad_estrella': 'st_age',
-			  'numero_planetas_estrella': 'pl_pnum', 'numero_lunas_planeta': 'pl_mnum',
-			  'año_descubrimiento_planeta': 'pl_disc', 'masa_planeta': 'pl_bmassj', 
-			  'radio_planeta': 'pl_rade', 'densidad_planeta': 'pl_dens'}
 
-	for i in kargs:
-		filt_data=filt_data[(filt_data[equivalencia[i]]>kargs[i][0]) & (filt_data[equivalencia[i]]<kargs[i][1])]
 	
-	return filt_data.pl_name.to_list()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
