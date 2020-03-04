f2060449fa3da50c633cce250fdd433a6f71b4fd
carlosmcastro <56790554+carlosmcastro@users.noreply.github.com>
2020-03-04 06:51:38 -0300

    Morgan Kennan a Lenguaje común
    Notación taxonomica de estrellas a lenguaje común.

---
+++ trasnformaciones.py
@@ -0,0 +1,18 @@
+#encoding: utf-8
+
+from container_constantes import MORGAN_KENNAN, EQUIVALENCIAS
+
+#Convierte la codificación de la clasificación Morgan-Kennan para estrellas,
+#en lenguaje común.
+# coloquial_mk('sdBV')	#Subdwarf star Blue-white Main-Sequence
+# coloquial_mk('K0/1 V + G (III)') #Yellow Orange Main-Sequence regular-giant
+def coloquial_mk(morgank):		
+	letras_Mk = [MORGAN_KENNAN['letras'][i] for i in set(morgank).intersection(MORGAN_KENNAN['letras'])]
+	num_romanos = [MORGAN_KENNAN['numeros'][u] for u in ''.join([i if i in 'IV' else " " for i in morgank]).split()]
+	return [morgank, " ".join(letras_Mk+num_romanos)]
+			
+
+			
+
+
+				
