128c3953874e9559034b00e4afa9e470d407b616
carlosmcastro <56790554+carlosmcastro@users.noreply.github.com>
2020-03-07 01:49:47 -0300

    LImites de masa y radio para estrellas y planetas.

--- transformaciones.py
+++ transformaciones.py
@@ -1,7 +1,7 @@
 #encoding: utf-8
 
-from container_constantes import MORGAN_KENNAN, EQUIVALENCIAS, TEMPERATURA_SOLAR
-from decimal import Decimal as D
+from container_constantes import D, ASTRO_DATA, LIMITES_PL_REALISTA, MORGAN_KENNAN, EQUIVALENCIAS, TEMPERATURA_SOLAR, TYP_DATOS, CONST_DMR
+import pickle
 
 #Convierte la codificación de la clasificación Morgan-Kennan para estrellas,
 #en lenguaje común.
@@ -30,7 +30,68 @@ def chz(stlum):
 	K_lum = D(str(stlum)).sqrt()
 	limite_interno = K_lum*D('0.75')	#UA
 	limite_externo = K_lum*D('1.77')	#UA
-	return [limite_interno, limite_externo, limite_externo-limite_interno]			
+	return [limite_interno, limite_externo, limite_externo-limite_interno]
+
+#####################################################################################################
+#####################################################################################################
+#####################################################################################################
+	
+#t: 'Estrella' o 'Planeta'
+		
+#a:masa
+#b:radio
+def dens(t, a, b):
+	f =  D(str(a))*D(str(b))**-3*CONST_DMR[t][0]
+	return f.scaleb(CONST_DMR[t][1])	#gr/cm**3
+	
+#a:densidad
+#b:radio
+def mass(t, a, b):
+	f = (D(str(a))*D(str(b))**3)/CONST_DMR[t][0]
+	return f.scaleb(-CONST_DMR[t][1])	#masa en masas solares/de jupiter.
+	
+
+#a:densidad
+#b:masa
+def radio(t, a, b):
+	f = ((D(str(b))*CONST_DMR[t][0])/D(str(a)))**(D(1)/D(3))
+	return f.scaleb(CONST_DMR[t][1]//3)	#radio en radios solares/terrestres.	
+
+#####################################################################################################
+#####################################################################################################
+#####################################################################################################
+
+
+def interval(t, masa=[], aux_mass=[]):
+	with open(LIMITES_PL_REALISTA, 'rb') as f:
+		nel = pickle.load(f)
+	if aux_mass:
+		tmass_min = nel[t]['proporcional_masa'][0]*aux_mass
+		tmass_max = nel[t]['proporcional_masa'][1]*aux_mass
+		return max(nel[t]['masa'][0], tmass_min), min(nel[t]['masa'][1], tmass_max)
+	if masa:
+		trad_min = radio(t, nel[t]['densidad'][1], masa)
+		trad_max = radio(t, nel[t]['densidad'][0], masa)
+		return max(nel[t]['radio'][0], trad_min), min(nel[t]['radio'][1], trad_max)
+	else:
+		return nel[t]['masa']
+	
+		
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
 
 			
 
